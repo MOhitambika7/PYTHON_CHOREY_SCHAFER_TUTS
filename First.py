65cd8dfd8e428b282a5a065a4c006bde192d3988
MOhitambika7 <mohitambikanand7@gmail.com>
2024-10-29 16:45:08 +0545

    This is the first python code

---
+++ First.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print("This is the beginning of the python programming language")
+message = 'PYTHON'
+print(message)
+print(len(message))
+print(message[0])
+print(message[5])
+print(message[0:2])
