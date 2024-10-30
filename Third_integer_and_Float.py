bcda107239016f34c94f2b8f7eb4de93a6f8fbaf
MOhitambika7 <mohitambikanand7@gmail.com>
2024-10-30 21:29:10 +0545

    In this tutorial we have discussed about working on numeric data types

---
+++ Third_integer_and_Float.py
@@ -0,0 +1,68 @@
+'''This tutorial is for working with numeric data.'''
+num = 3
+print(type(num))
+
+value = 4.5656
+print(type(value))
+
+#Arithmetic operators:-
+# Addition :   3 + 2
+#Subtraction:- 3 -2
+# Multiplication:- 3*2
+# Division:- 3 / 2
+# Floor Division:- 3 // 2
+# Exponent:- 3 ** 2
+# Modulus:-  3 % 2
+
+print(3 +2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2) 
+print(3 // 2)
+print(3 ** 2)
+print(3 % 2)
+print(4 // 2 + 3) 
+
+num = num + 5
+print(num)
+
+
+Value = 5
+Value += 10
+print(Value)
+
+print(abs(-3)) #You can use abs() with both integers and floating-point numbers, and it’s useful in cases where you need the magnitude of a value without considering its sign.
+
+print(round(3.75))
+print(round(3.75, 1))
+print(round(3.75, 2))
+print(round(3.8, 2))
+
+
+#comparison
+#Equal:- ==
+#Not Equal:- !=
+#Greater Than:- >
+#Less Than:- <
+#Greater or Equal:->=
+#Less or Equal:- <=
+
+num_1 = 3
+num_2 = 4
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 > num_2)
+print(num_1 < num_2)
+print(num_1 >= num_2)
+print(num_1 <= num_2)
+
+NUM_1 ='100'
+NUM_2 = '200'
+print(NUM_1 + NUM_2)
+
+print(type(NUM_1))
+print(type(NUM_2))
+
+NUM_1 = int(NUM_1)
+NUM_2 = int(NUM_2)
+print(NUM_1 + NUM_2)
